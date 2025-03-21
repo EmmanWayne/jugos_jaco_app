d3a01386826b89a7107b277fa84dc04dc65b8e8f
justin2070 <43504797+justin2070@users.noreply.github.com>
2025-03-21 12:09:54 -0600

    Revert "cambios"
    This reverts commit 66841138e1a5166f3f0a882a1f2b184af811f761.

--- app/src/main/java/com/jugos_jaco_app/ui/api/ApiService.java
+++ app/src/main/java/com/jugos_jaco_app/ui/api/ApiService.java
@@ -30,12 +30,4 @@ public interface ApiService {
         @Path("id") int mediaId,
         @Header("Authorization") String token
     );
-
-    @Multipart
-    @POST("{id}/image/profile")
-    Call<PhotoResponse> uploadProfileImage(
-        @Path("id") String clientId,
-        @Part MultipartBody.Part image,
-        @Header("Authorization") String token
-    );
 } 
